7132fa67ec5841203d414bebfbbee61bc4115542
SNprime24 <supritnaik100@gmail.com>
2025-07-31 15:17:39 +0530

    modified the runner fileto pass diagram name by the argument

--- runner.bash
+++ runner.bash
@@ -1,7 +1,11 @@
 g++ REtoNFA.cpp -o nfa.exe
-
 ./nfa.exe
 
-node printAutomata.js nfa_diagram.mmd nfa_diagram.svg
+outputBase="$1"
+if [ -z "$outputBase" ]; then
+  outputBase="nfa_diagram"
+fi
+
+node printAutomata.js nfa_diagram.mmd "$outputBase.svg"
 
 echo "SVG generated: nfa_diagram.svg"
